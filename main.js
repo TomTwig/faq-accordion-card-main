00281277e8498144a143bb8308e2200480be200d
Tom Rosenzweig <tom.rosenzweig@live.de>
2022-09-01 16:50:25 +0200

    add arrow animation

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 const answers = document.querySelectorAll(".card__info__content-answer");
 const arrows = document.querySelectorAll(".arrow");
 
-console.log(arrows);
 
 arrows.forEach(element => {
-
     element.addEventListener("click",showQuestion);
-
 })
 
 
@@ -14,16 +11,16 @@ arrows.forEach(element => {
 function showQuestion(element){
 
     if(element.target.parentNode.nextSibling.nextSibling.style.display === "none"){
+
         element.target.parentNode.style.fontWeight = "bold";
-        element.target.style.transform = "rotate(180deg)"
+        element.target.style.animationName =  "rotateOpen";
         element.target.parentNode.nextSibling.nextSibling.style.display = "block";
 
     }else{
+
         element.target.parentNode.style.fontWeight = "normal";
-        element.target.style.transform = "rotate(0deg)"
-
+        element.target.style.animationName =  "rotateClose";
         element.target.parentNode.nextSibling.nextSibling.style.display = "none";
+        
     }
-    
-
 }
