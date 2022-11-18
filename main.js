30a409ad071f2f8b057bf396043e1f83eff47e69
Tom Twig <tom.rosenzweig@live.de>
2022-11-18 21:33:51 +0100

    fix arrow animation on load bug

--- main.js
+++ main.js
@@ -5,21 +5,27 @@ const arrows = document.querySelectorAll(".arrow");
 arrows.forEach(element => {
     element.addEventListener("click",showQuestion);
 })
-
-
+answers.forEach(element => {
+    element.style.display="none";
+ 
+})
 
 function showQuestion(element){
 
-    if(element.target.parentNode.nextSibling.nextSibling.style.display === "none"){
+    if(element.target.parentNode.nextSibling.nextSibling.style.display == "none"){
+
+       
 
         element.target.parentNode.style.fontWeight = "bold";
-        element.target.style.animationName =  "rotateOpen";
+        element.target.classList.add("rotateOpen");
+        element.target.classList.remove("rotateClose");
         element.target.parentNode.nextSibling.nextSibling.style.display = "block";
 
     }else{
 
         element.target.parentNode.style.fontWeight = "normal";
-        element.target.style.animationName =  "rotateClose";
+        element.target.classList.remove("rotateOpen");
+        element.target.classList.add("rotateClose");
         element.target.parentNode.nextSibling.nextSibling.style.display = "none";
         
     }
